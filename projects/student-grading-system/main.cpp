3ec8f9898a3eb4d64e6e0e79a5d57dc7aa83db84
Harrison684 <harrisonmunene837@gmail.com>
2026-09-24 16:41:59 +0300

    Implement student grading system in main.cpp

---
+++ projects/student-grading-system/main.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+int main(){
+  count<<"Enter your marks:";
+cin>> marks;
+if(marks>=70){
+cout<<"Excellent";
+}
+else if(marks>=60){
+cout<<"Good";
+}
+else if(marks>=50){
+cout<<"Average";
+}
+else if(marks>=40){
+cout<<"Pass";
+}
+else{
+cout<<"Fail";
+}
+return 0;
+}
